eb72788540cbacb113b5164dc3774eaced4302ab
Stijntjeuh <stijntjeuh123@gmail.com>
2022-04-29 16:17:10 +0200

    Homing bullet now stops once it has gotten close

--- Astral Drift/Assets/Stijn/Scripts/HomingBullet.cs
+++ Astral Drift/Assets/Stijn/Scripts/HomingBullet.cs
@@ -11,12 +11,10 @@ public class HomingBullet : Bullet
     private Quaternion lookRotation;
     private float lerpTime;
 
-    private Rigidbody2D rb;
-    [SerializeField]private bool useRigidbody;
     private float currentHomingStartTimer;
     private float homingStartTime;
     private float stopHomingDistance;
-    private bool shouldBeHoming = true;
+    private bool shouldBeHoming;
 
     public HomingBullet(string collisionTag, Vector3 position, Vector3 direction, float speed, int damage, float lifespan) : base(collisionTag, position, direction, speed, damage, lifespan)
     {
@@ -26,23 +24,17 @@ public class HomingBullet : Bullet
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag(collisionTag);
         direction = transform.up;
         homingStartTime = 0.5f;
-        stopHomingDistance = 0.01f;
+        stopHomingDistance = 0.5f;
         lerpTime = 0;
+        shouldBeHoming = true;
     }
 
     //Override fixedupdate to change the direction of the projectile
     protected override void FixedUpdate()
     {
-        if (useRigidbody) 
-        {
-            HomingRigidbody();
-        }
-        else
-        {
             if (currentHomingStartTimer < homingStartTime)
             {
                 currentHomingStartTimer += 0.01f;
@@ -54,8 +46,6 @@ public class HomingBullet : Bullet
 
             //Move the projectile forward
             base.FixedUpdate();
-        }
-
     }
 
     //Rotate the projectile towards the target
@@ -68,45 +58,31 @@ public class HomingBullet : Bullet
                 directionToTarget = target.transform.position - this.transform.position;
 
                 lookRotation = Quaternion.LookRotation(directionToTarget);
-                //Debug.Log(directionToTarget.magnitude);
-                
 
-                if (directionToTarget.magnitude >= stopHomingDistance)
+                //Stop rotating once the missile has been close once
+                if (directionToTarget.magnitude >= stopHomingDistance && shouldBeHoming)
                 {
-                        //Lerp from current rotation to lookrotation
-                        if (lerpTime < 1)
-                        {
+                    //Lerp from current rotation to lookrotation
+                    if (lerpTime < 1)
+                    {
                             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, lerpTime);
                             transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
                             lerpTime += Time.deltaTime * rotatingSpeed;
-                        }
-                        else
-                        {
-                            lerpTime = 0;
-                        }
+                    }
+                    else
+                    {
+                         lerpTime = 0;
+                    }
                 }
                 else
                 {
                     shouldBeHoming = false;
-                    //Debug.Log("STOP HOMING");
                 }
             }
         }
     }
 
-    private void HomingRigidbody()
-    {
-        Vector2 newdirection = (Vector2)target.transform.position - rb.position;
-
-        newdirection.Normalize();
-
-        float rotateAmount = Vector3.Cross(newdirection, -transform.up).z;
-
-        rb.angularVelocity = -rotateAmount * 200;
-            
-        rb.velocity = -transform.up * 1;
-    }
-
+    //Get the angle between two vectors. This function also gets negative angles
     private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
     {
         Vector2 diference = vec2 - vec1;
