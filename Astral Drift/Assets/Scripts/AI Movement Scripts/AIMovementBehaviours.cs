85de0542ed5a7681564d7f3321f4325d5f48b678
schuddn <nicolaas.schuddeboom@hva.nl>
2022-05-11 10:28:02 +0200

    Fixed get hit bug and added more enemies to testing scene.

--- Astral Drift/Assets/Scripts/AI Movement Scripts/AIMovementBehaviours.cs
+++ Astral Drift/Assets/Scripts/AI Movement Scripts/AIMovementBehaviours.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AIMovementBehaviours : MonoBehaviour
 {
+    protected int currentHitpoints;
+
     public virtual void MoveRight(Transform movingObject, float speed)
     {
         movingObject.position += new Vector3(1 * speed, 0, 0);
@@ -26,4 +28,17 @@ public class AIMovementBehaviours : MonoBehaviour
         Vector3 offset = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0) * radius;
         movingObject.position = startPosition + offset;
     }
+
+    //Enemy gets hit got hit
+    public void GetHit(int damage)
+    {
+        currentHitpoints -= damage;
+
+        if (currentHitpoints <= 0)
+        {
+            //TO DO: Enemy dies
+            gameObject.SetActive(false);
+        }
+    }
+
 }
