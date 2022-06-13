e842f6ec535c4307db345d646dc7f0b230ce676d
Tristan <tristanmul@gmail.com>
2022-06-13 11:16:56 +0200

    Start working on overhaul

--- Astral Drift/Assets/GameObjects/Enemies/Building blocks/scripts/GunBarrels/LaserBarrel.cs
+++ Astral Drift/Assets/GameObjects/Enemies/Building blocks/scripts/GunBarrels/LaserBarrel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserBarrel : MonoBehaviour
+public class LaserBarrel : BaseGunBarrel
 {
     [SerializeField] private LineRenderer aimingLaserRenderer;
 
