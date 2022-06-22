ed49e68c8877344007ed4e9f4707a0f67731a6b2
schuddn <nicolaas.schuddeboom@hva.nl>
2022-06-22 11:26:32 +0200

    Fixed HealthBar bug where previously, the mask would move over another health bar and cause a visual bug. Enemy health bars now change in size instead.

--- Astral Drift/Assets/UIObjects/Scripts/healthBarUI.cs
+++ Astral Drift/Assets/UIObjects/Scripts/healthBarUI.cs
@@ -10,30 +10,15 @@ public class HealthBarUI : MonoBehaviour
 
     [SerializeField] protected GameObject fullBar, mask;
     [Tooltip("Edit this value to the current size of the sprite. (in canvas this is the width and height)")]
-    [SerializeField] private float spriteSize = 686;
-
-    [Tooltip("False if this is in the canvas. For now, This will be used a lot outside of it and needs to be true.)")]
-
-    private int percentage;
-    
-    protected float amount;
+    [SerializeField] protected float spriteSize = 686;
 
     protected virtual void Start()
     {
         healthScript.OnHitEvent.AddListener(UpdateHealthBar);
     }
 
-    private void UpdateHealthBar()
+    protected virtual void UpdateHealthBar()
     {
-        percentage = healthScript.CurrentHitpoints;
-        if (percentage <= 0)
-        {
-            gameObject.SetActive(false);
-        }
-
-        amount = (healthScript.maxHitpoints - percentage) * spriteSize / healthScript.maxHitpoints;
-
-        fullBar.transform.localPosition = new Vector3(0, amount, 0);
-        mask.transform.localPosition = new Vector3(0, -amount, 0);
+        // This function is used for inheritance.
     }
 }
