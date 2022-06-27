2d2a6f285b60aa1865803a6b7ccdf23af14edef7
Omega_The_III <jeroen.officier@hva.nl>
2022-06-27 14:22:19 +0200

    After merge fix

--- Astral Drift/Assets/GameObjects/Pickupables/BuildingBlocks/scripts/ScorePickupable.cs
+++ Astral Drift/Assets/GameObjects/Pickupables/BuildingBlocks/scripts/ScorePickupable.cs
@@ -1,32 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScorePickupable : Pickupable
 {
     [SerializeField] private int scoreIncrease = 1;
-    private Rigidbody2D rigidbody;
-    private float randomDirectionX, randomDirectionY;
-    [SerializeField] private GameObject starParticle;
     [SerializeField] private float starAnimationTimer = 1f;
 
     public override void OnPickUp(Collider2D collision)
-    private void Start()
     {
         base.OnPickUp(collision);
-        rigidbody = GetComponent<Rigidbody2D>();
-        randomDirectionX = Random.Range(-3, 3);
-        randomDirectionY = Random.Range(-3, 3);
-        rigidbody.AddForce(new Vector2(randomDirectionX, randomDirectionY), ForceMode2D.Impulse);
-    }
-
-    protected override void PickUp(Collider2D collision)
-    {
-        base.PickUp(collision);
 
         GlobalReferenceManager.AudioManagerRef.PlaySound("StarPickup");
 
-        GameObject starPrefab = Instantiate(starParticle, transform.position, transform.rotation);
+        GameObject starPrefab = Instantiate(particlePrefab, transform.position, transform.rotation);
         Destroy(starPrefab, starAnimationTimer);
         UI.instance.AddScore(scoreIncrease);
     }
