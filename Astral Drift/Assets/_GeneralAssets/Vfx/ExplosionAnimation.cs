fa340869f73deaec71e96b9204145cb7a3e5f8fa
Mathijs Marks <mathijsmarks@msn.com>
2022-06-30 14:04:54 +0200

    Commented ExplosionAnimation.cs

--- Astral Drift/Assets/_GeneralAssets/Vfx/ExplosionAnimation.cs
+++ Astral Drift/Assets/_GeneralAssets/Vfx/ExplosionAnimation.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// On death, an explosion animation is played.
+/// </summary>
 public class ExplosionAnimation : MonoBehaviour
 {
     
     private void Start()
     {
+        // Destroy the game object after the animation has run its course.
         Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
     }
     
